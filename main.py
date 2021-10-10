be4eabee6893a973df1c65f8118cadd86744e4e7
ozan12341 <34062337+ozan-git@users.noreply.github.com>
2021-10-10 18:57:18 +0300

    The application has been made operational.

--- main.py
+++ main.py
@@ -197,10 +197,9 @@ Enter Value: """)
 				start_merge_insert = time.perf_counter()
 				MergeInsertionSort(arr).sorting()
 				end_merge_insert = time.perf_counter()
-				print(len(arr), "Elements Sorted by MergeSort Based Insertion in ",
-					  end_merge_insert - start_merge_insert)
+				print(len(arr), "Elements Sorted by MergeSort Based Insertion in ",  end_merge_insert - start_merge_insert)
 				elements_merge_insert.append(len(arr))
-				times_merge_insert.append(MergeInsertionSort(arr).sorting())
+				times_merge_insert.append(end_merge_insert - start_merge_insert)
 
 			plt.xlabel('List Length')
 			plt.ylabel('Time Complexity')
